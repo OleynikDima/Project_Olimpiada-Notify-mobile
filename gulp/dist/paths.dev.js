a6560ea4f2cc67c7d5d4a0d1cfba9e5fa754a7b3
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-08-22 21:40:39 +0300

    fix scss and add list people face ul

--- gulp/dist/paths.dev.js
+++ gulp/dist/paths.dev.js
@@ -1,29 +1,31 @@
+"use strict";
+
 module.exports = {
   src: {
     html: 'src/*.html',
     css: 'src/scss/main.scss',
     js: 'src/js/**/*.js',
     images: 'src/images/**/*',
-    fonts: 'src/fonts/**/*',
+    fonts: 'src/fonts/**/*'
   },
   watch: {
     html: 'src/**/*.html',
     css: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     images: 'src/images/**/*',
-    fonts: 'src/fonts/**/*',
+    fonts: 'src/fonts/**/*'
   },
   build: {
     html: 'build/',
     css: 'build/css',
     js: 'build/js',
     images: 'build/images',
-    fonts: 'build/fonts',
+    fonts: 'build/fonts'
   },
   inject: {
     html: 'build/index.html',
     css: 'build/css/**/*.css',
-    js: 'build/js/**/*.js',
+    js: 'build/js/**/*.js'
   },
-  clean: 'build/',
+  clean: 'build/'
 };
